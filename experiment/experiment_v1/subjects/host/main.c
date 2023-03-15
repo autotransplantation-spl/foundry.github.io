8f6e5ee9ccb9d3f9cfbf853845e4a59e356ff1aa
autotransplantation-spl <leandro.souza@ifba.edu.br>
2023-03-15 04:28:00 -0300

    upload experiment artifacts

--- experiment/experiment_v1/subjects/host/main.c
+++
@@ -1,5 +0,0 @@
-void main(){
-    char *tarArchive = (char *) malloc (1000 * sizeof (char));
-    char *in_file = (char *) malloc (1000 * sizeof (char));
-    __ADDGRAFTHERE__JUSTHERE
-}
--- experiment/experiment_v2/Organ_spell_check/uninstal.c
+++
@@ -1,6 +0,0 @@
-static int confirm (void) {
-    char answer [10];
-    fflush (stdout);
-    return (scanf (" %c", answer) == 1 && toupper (answer[0]) == 'Y');
-}
-
